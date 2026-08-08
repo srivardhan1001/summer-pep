75ab16c777094100d316d37d3cbabf682a99517d
TAMMINAINA SRI VARDHAN <150380589+srivardhan1001@users.noreply.github.com>
2026-08-08 15:53:35 +0530

    Delete question1.py

--- question1.py
+++
@@ -1,7 +0,0 @@
-def create_paginator(items,pageSize):
-    for i in range(0,len(items),pageSize):
-        yield items[i:i + pageSize]
-items = list(map(int, input("Enter items: ").split()))
-pageSize = int(input("Enter page size: "))
-for page in create_paginator(items,pageSize):
-    print(page)
